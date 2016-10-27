187da20d1f5d18b0dae35f0eb248f081fd5f9fce
eabyers1 <EnochByers@gmail.com>
2016-10-27 01:46:37 -0500

    Update ServoLum.py

--- ServoLum.py
+++ ServoLum.py
@@ -2,6 +2,8 @@ import serial
 import datetime
 import time
 import csv
+
+# This code runs on a raspberry pi and communicates with an Arduino to control the light source for the GONetwork.
 ser = serial.Serial('/dev/ttyUSB0',9600, timeout=1)
 lum=[0,1]
 while True:
@@ -10,19 +12,13 @@ while True:
 	min = t[4];
 	hr = t[3];
 	today = datetime.datetime.now()
+# Depending on the time, the Pi sends a signal to the Arduino to either rotate the servo clockwise or counterclockwise
 	if secs == 28 or secs == 58:
-	# 2 turns clockwise
 		read_serial=ser.write('1')
 		print(t);
 		print(secs);
 		time.sleep(1)
-		#read_serial=ser.readline();
-                #lum[0] = str(ser.readline())
-                #with open('/home/pi/luminositylog.csv','a') as csvfile:
-                #        csvwriter = csv.writer(csvfile,delimiter=',',quotechar=$
-                #        csvwriter.writerow([lum,today]);
 
-                #continue
 	if secs == 44 or secs == 14:
 		read_serial=ser.readline();
 		lum[0] = str(ser.readline()) 
@@ -31,9 +27,4 @@ while True:
 			csvwriter.writerow([lum,today]);
 		
 		continue		
-#	if secs == 13 or secs == 43:
-	# 1 turns counter clockwise	
-#		read_serial=ser.write('1')
-#		print(t);
-#		print(secs);
-#		time.sleep(1);
+
